9cddd692fb9bbb5a9339480577da98d0727d3b9e
Andrey Savka <a_sawka@yahoo.com>
2026-04-20 20:19:02 +0300

    optimize menu

--- admin/src/components/AdminLayout.tsx
+++ admin/src/components/AdminLayout.tsx
@@ -35,11 +35,6 @@ const AdminLayout = ({ children }: AdminLayoutProps) => {
             <div className="collapse navbar-collapse">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    Панель
-                  </Link>
-                </li>
-                <li className="nav-item">
                   <Link className="nav-link" to="/steps">
                     Шаги
                   </Link>
